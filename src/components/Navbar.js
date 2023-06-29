0dd0a225ffd5bf110311bf6ab59e360d6f9fde6d
davidebalice <davide.balice@gmail.com>
2023-06-29 18:03:27 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { HiShoppingCart } from "react-icons/hi";
 import logo from "../assets/logo_white.png";
 import { useGlobalContext } from "../context/context";
@@ -8,13 +8,37 @@ import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   const { isLoading, itemCounter } = useGlobalContext();
+  const cartRef = useRef(null);
+
+  useEffect(() => {
+    if (itemCounter >= 1) {
+      if (cartRef.current) {
+        cartRef.current.classList.add(classes.cartZoom);
+      }
+
+      const animationEndHandler = () => {
+        cartRef.current.classList.remove(classes.cartZoom);
+      };
+
+      cartRef.current.addEventListener("animationend", animationEndHandler);
+
+      return () => {
+        if (cartRef.current) {
+          cartRef.current.removeEventListener(
+            "animationend",
+            animationEndHandler
+          );
+        }
+      };
+    }
+  }, [itemCounter]);
 
   return (
     <nav className="nav">
       <header className="nav-header">
         <Row className={classes.NavSx}>
           <NavLink to="/">
-              <img src={logo} alt="DB logo" className="logo" />
+            <img src={logo} alt="DB logo" className="logo" />
           </NavLink>
           <NavLink to="/" className={classes.home}>
             <div className={classes.NavHome}>Home</div>
@@ -23,7 +47,11 @@ const Navbar = () => {
         <NavLink to="/cart" className={classes.NavLink}>
           <div className="nav-cart">
             <HiShoppingCart className={classes.cartIcon + " icon nav-icon"} />
-            {!isLoading && <div className={classes.cartNum}>{itemCounter}</div>}
+            {!isLoading && (
+              <div ref={cartRef} className={classes.cartNum}>
+                {itemCounter}
+              </div>
+            )}
           </div>{" "}
         </NavLink>
       </header>
